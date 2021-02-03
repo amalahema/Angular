22a546f6adb545b256a31c248c026b13b9b6cbd9
Amala <amalahema@gmail.com>
2021-02-03 15:40:25 -0600

    Data Binding - Angular

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,14 +10,20 @@ import { DISHES } from '../shared/dishes';
 
 export class MenuComponent implements OnInit {
   dishes: Dish[] = DISHES;
-  selectedDish = DISHES[0];//used to diaplay the 1st item of the array
-   
+  selectedDish :Dish;//used to diaplay the 1st item of the array
+ //equals signs are used to assign the value to a variable. A colon is used to assign a type.
   constructor() { }
 
   ngOnInit() {
   }
+  onSelect(dish: Dish)//parameter dish is passed to select method
+   {
+    this.selectedDish = dish;//dish is assigned to selectedDish
+   }
 
 }
+
+
  
  
  
