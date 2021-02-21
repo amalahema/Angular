8dab848ccc1303e39e37ffff79cd66eb50ea4498
Amala <amalahema@gmail.com>
2021-02-21 14:37:34 -0600

    Reactive Form & Validation - Part 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 //The FormBuilder is the helper API to build forms in Angular.  It provides shortcuts to create the instance of the FormControl, FormGroup or FormArray. It reduces the code required to write the complex forms.
 import { Feedback, ContactType } from '../shared/feedback';
-
-@Component({
+//Viewchild will enable us to get access to any of the child dom elements within my template.
+@Component({//Amgular directive
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
@@ -12,10 +12,11 @@ export class ContactComponent implements OnInit {
   feedbackForm: FormGroup;//Form module to host the reactive form
   feedback: Feedback;//corresponding data model(fetched from the server )
   contactType = ContactType;
-
-  constructor(private formbuilder: FormBuilder) 
-  //called when the component is constructed & Used for Injecting dependencies
-  //Construction is first, but happens when the component isn't really a component yet. So therefore the constructor should only contain very basic simple code relating to basic initialization of the class. You will have the injected services
+  @ViewChild('fform')feedbackFormDirective;
+  //Template variable fform to refer feedbackform,enables access to the complete template from
+  constructor(private formbuilder: FormBuilder) //create a referenct to formbuilder and access throughout the components by Injecting dependencies
+  //Construction is first, but happens when the component isn't really a component yet. 
+  //So therefore the constructor should only contain very basic simple code relating to basic initialization of the class. You will have the injected services
   { 
     this.createForm();
   }
@@ -28,10 +29,10 @@ export class ContactComponent implements OnInit {
     {
     //Create a Form Group
     this.feedbackForm = this.formbuilder.group({
-      firstname: '',
-      lastname: '',
-      telnum: 0,
-      email: '',
+      firstname: ['',Validators.required],
+      lastname: ['',Validators.required],
+      telnum: [0,Validators.required],
+      email: ['',Validators.required],
       agree: false,
       contacttype: 'None',
       message: ''
@@ -41,6 +42,21 @@ export class ContactComponent implements OnInit {
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum:0,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+
+    });
+    this.feedbackFormDirective.resetForm();
   }
 }
+/*why we inject form builder instead of declare it?
+Dependency injections is a mechanism that provides you with references where you need them. 
+Imagine a class that represents a connection pool to your database 
+you usually only have one instance of that class. 
+Now you need to distribute that reference to all the classes that use it. Here is where Dependency Injection comes in handy*/
